16df8411cff787255d5fa1c51651d98b6a6f7f74
vm <vicki.mondino@hotmail.com>
2024-12-13 11:18:22 -0300

    date

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import "./navbarar.css";
+import "./navbar.css";
 
 const NavBar = () => {
   return (
